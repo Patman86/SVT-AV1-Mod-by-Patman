7f5caeeac240a8bfb29ffb994e2e4b12a7028d5f
Patman86 <suckzpsych0@gmail.com>
2025-09-07 11:37:46 +0200

    optimization the structure of EbSvtAv1EncConfiguration

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -21,6 +21,13 @@ extern "C" {
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+
+#if defined(_MSC_VER)
+#define ALIGNED(n) __declspec(align(n))
+#else
+#define ALIGNED(n) __attribute__((aligned(n)))
+#endif
+
 /**
  * @brief SVT-AV1 encoder ABI version
  *
@@ -215,7 +222,7 @@ typedef struct SvtAv1FrameScaleEvts {
 
 // Will contain the EbEncApi which will live in the EncHandle class
 // Only modifiable during config-time.
-typedef struct EbSvtAv1EncConfiguration {
+typedef struct ALIGNED(128) EbSvtAv1EncConfiguration {
     /**
      * @brief Encoder preset used.
      * -2 and -1 are for debug purposes and should not be used.
@@ -1067,10 +1074,6 @@ typedef struct EbSvtAv1EncConfiguration {
      */
      bool adaptive_film_grain;
 
-    /*Add 128 Byte Padding to Struct to avoid changing the size of the public configuration struct*/
-    uint8_t padding[128 - (sizeof(uint8_t) * 9) - (sizeof(double) * 2) - (sizeof(bool) * 3)
-        - sizeof(bool)
-    ];
 } EbSvtAv1EncConfiguration;
 
 /**
