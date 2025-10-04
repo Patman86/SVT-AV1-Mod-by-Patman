8fe4be7306898a922791f0666e99f4a9102c1be3
Uranite <62639703+Uranite@users.noreply.github.com>
2025-10-04 18:59:04 +0700

    Auto tiling from Essential
    Enabled by default

--- Source/App/app_config.c
+++ Source/App/app_config.c
@@ -225,6 +225,7 @@
 #define COMPLEX_HVS_TOKEN "--complex-hvs"
 #define NOISE_ADAPTIVE_FILTERING_TOKEN "--noise-adaptive-filtering"
 #define CDEF_SCALING_TOKEN "--cdef-scaling"
+#define AUTO_TILING_TOKEN "--auto-tiling"
 
 static EbErrorType validate_error(EbErrorType err, const char* token, const char* value) {
     switch (err) {
@@ -944,6 +945,8 @@ ConfigDescription config_entry_intra_refresh[] = {
     {NULL, NULL}};
 
 ConfigDescription config_entry_specific[] = {
+    // Auto tiling
+    {AUTO_TILING_TOKEN, "Auto tiling, default is 1 [0-1]"},
     {TILE_ROW_TOKEN, "Number of tile rows to use, `TileRow == log2(x)`, default changes per resolution but is 1 [0-6]"},
     {TILE_COL_TOKEN,
      "Number of tile columns to use, `TileCol == log2(x)`, default changes per resolution but is 1 [0-4]"},
@@ -1359,6 +1362,9 @@ ConfigEntry config_entry[] = {
 
     // CDEF scaling
     {CDEF_SCALING_TOKEN, "CDEFScaling", set_cfg_generic_token},
+
+    // Auto tiling
+    {AUTO_TILING_TOKEN, "AutoTiling", set_cfg_generic_token},
 
     // Termination
     {NULL, NULL, NULL}};
