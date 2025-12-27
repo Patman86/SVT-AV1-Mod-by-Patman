f3e98cddacb291633f4750719c738eb1dc483668
Patman86 <suckzpsych0@gmail.com>
2025-12-26 22:10:08 -0800

    optimize handling of Dolby Vision/ HDR10+ library

--- cmake/Finddovi.cmake
+++ cmake/Finddovi.cmake
@@ -1,31 +1,67 @@
 # Find Dolby Vision (dovi) library
 
 get_filename_component(_SVT_ROOT "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
-if (MSVC)
-    set(_DOVI_PLATFORM_DIR "msvc")
+
+if(MSVC)
+    set(_DOVI_COMP_DIR "msvc")
 else()
-    set(_DOVI_PLATFORM_DIR "gnu")
+    set(_DOVI_COMP_DIR "gnu")
 endif()
 
-set(_DOVI_HINT_PATHS
-    "${_SVT_ROOT}/source/dovi/${_DOVI_PLATFORM_DIR}"
-    "${_SVT_ROOT}/source/dovi/${_DOVI_PLATFORM_DIR}/lib"
-    "${_SVT_ROOT}/source/dovi/lib"
-    "${_SVT_ROOT}/source/dovi")
+set(_DOVI_PROJECT_HINTS
+    "${_SVT_ROOT}/dovi/${_DOVI_COMP_DIR}/lib"
+    "${_SVT_ROOT}/dovi/${_DOVI_COMP_DIR}"
+    "${_SVT_ROOT}/dovi/lib"
+    "${_SVT_ROOT}/dovi"
 
-find_path(LIBDOVI_INCLUDE_DIR NAMES libdovi/rpu_parser.h HINTS ${_DOVI_HINT_PATHS} PATH_SUFFIXES include)
-find_library(LIBDOVI_LIBRARY NAMES dovi libdovi dovi_static HINTS ${_DOVI_HINT_PATHS} PATH_SUFFIXES lib lib64)
+    "${_SVT_ROOT}/Source/dovi/${_DOVI_COMP_DIR}/lib"
+    "${_SVT_ROOT}/Source/dovi/${_DOVI_COMP_DIR}"
+    "${_SVT_ROOT}/Source/dovi/lib"
+    "${_SVT_ROOT}/Source/dovi"
+
+    "${_SVT_ROOT}/third_party/dovi/${_DOVI_COMP_DIR}/lib"
+    "${_SVT_ROOT}/third_party/dovi/${_DOVI_COMP_DIR}"
+
+    "${_SVT_ROOT}/lib/dovi"
+    "${_SVT_ROOT}/libs/dovi"
+)
+
+set(_DOVI_INCLUDE_HINTS
+    "${_SVT_ROOT}/dovi/${_DOVI_COMP_DIR}/include"
+    "${_SVT_ROOT}/dovi/include"
+    "${_SVT_ROOT}/Source/dovi/${_DOVI_COMP_DIR}/include"
+    "${_SVT_ROOT}/Source/dovi/include"
+)
+
+find_path(LIBDOVI_INCLUDE_DIR
+    NAMES libdovi/rpu_parser.h
+    HINTS ${_DOVI_INCLUDE_HINTS}
+    PATH_SUFFIXES include libdovi
+)
+
+find_library(LIBDOVI_LIBRARY
+    NAMES dovi libdovi dovi_static
+    HINTS ${_DOVI_PROJECT_HINTS}
+    PATH_SUFFIXES lib lib64
+)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(dovi REQUIRED_VARS LIBDOVI_LIBRARY LIBDOVI_INCLUDE_DIR)
+find_package_handle_standard_args(dovi
+    REQUIRED_VARS LIBDOVI_LIBRARY LIBDOVI_INCLUDE_DIR
+)
 
-if (dovi_FOUND AND NOT TARGET dovi::dovi)
+if(dovi_FOUND AND NOT TARGET dovi::dovi)
     add_library(dovi::dovi UNKNOWN IMPORTED)
     set_target_properties(dovi::dovi PROPERTIES
         IMPORTED_LOCATION "${LIBDOVI_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${LIBDOVI_INCLUDE_DIR}")
+        INTERFACE_INCLUDE_DIRECTORIES "${LIBDOVI_INCLUDE_DIR}"
+    )
 endif()
 
 set(LIBDOVI_FOUND ${dovi_FOUND})
 set(LIBDOVI_LIBRARIES ${LIBDOVI_LIBRARY})
 mark_as_advanced(LIBDOVI_INCLUDE_DIR LIBDOVI_LIBRARY)
+
+if(LIBDOVI_FOUND)
+    message(STATUS "libdovi found: ${LIBDOVI_LIBRARY}")
+endif()
--- cmake/Findhdr10plus.cmake
+++ cmake/Findhdr10plus.cmake
@@ -1,31 +1,70 @@
 # Find HDR10+ library
 
 get_filename_component(_SVT_ROOT "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
-if (MSVC)
-    set(_HDR10PLUS_PLATFORM_DIR "msvc")
+
+if(MSVC)
+    set(_HDR_COMP_DIR "msvc")
 else()
-    set(_HDR10PLUS_PLATFORM_DIR "gnu")
+    set(_HDR_COMP_DIR "gnu")
 endif()
 
-set(_HDR10PLUS_HINT_PATHS
-    "${_SVT_ROOT}/source/HDR/${_HDR10PLUS_PLATFORM_DIR}"
-    "${_SVT_ROOT}/source/HDR/${_HDR10PLUS_PLATFORM_DIR}/lib"
-    "${_SVT_ROOT}/source/HDR/lib"
-    "${_SVT_ROOT}/source/HDR")
+set(_HDR10PLUS_PROJECT_HINTS
+    "${_SVT_ROOT}/hdr/${_HDR_COMP_DIR}/lib"
+    "${_SVT_ROOT}/hdr/${_HDR_COMP_DIR}"
+    "${_SVT_ROOT}/hdr/lib"
+    "${_SVT_ROOT}/hdr"
 
-find_path(LIBHDR10PLUS_RS_INCLUDE_DIR NAMES libhdr10plus-rs/hdr10plus.h HINTS ${_HDR10PLUS_HINT_PATHS} PATH_SUFFIXES include)
-find_library(LIBHDR10PLUS_RS_LIBRARY NAMES hdr10plus-rs libhdr10plus-rs hdr10plus_static HINTS ${_HDR10PLUS_HINT_PATHS} PATH_SUFFIXES lib lib64)
+    "${_SVT_ROOT}/Source/hdr/${_HDR_COMP_DIR}/lib"
+    "${_SVT_ROOT}/Source/hdr/${_HDR_COMP_DIR}"
+    "${_SVT_ROOT}/Source/hdr/lib"
+    "${_SVT_ROOT}/Source/hdr"
+
+    "${_SVT_ROOT}/Source/HDR/${_HDR_COMP_DIR}/lib"
+    "${_SVT_ROOT}/Source/HDR/${_HDR_COMP_DIR}"
+    "${_SVT_ROOT}/Source/HDR/lib"
+    "${_SVT_ROOT}/Source/HDR"
+
+    "${_SVT_ROOT}/third_party/hdr/${_HDR_COMP_DIR}/lib"
+    "${_SVT_ROOT}/third_party/HDR/${_HDR_COMP_DIR}/lib"
+)
+
+set(_HDR10PLUS_INCLUDE_HINTS
+    "${_SVT_ROOT}/hdr/${_HDR_COMP_DIR}/include"
+    "${_SVT_ROOT}/hdr/include"
+    "${_SVT_ROOT}/Source/hdr/${_HDR_COMP_DIR}/include"
+    "${_SVT_ROOT}/Source/hdr/include"
+    "${_SVT_ROOT}/Source/HDR/${_HDR_COMP_DIR}/include"
+)
+
+find_path(LIBHDR10PLUS_RS_INCLUDE_DIR
+    NAMES libhdr10plus-rs/hdr10plus.h
+    HINTS ${_HDR10PLUS_INCLUDE_HINTS}
+    PATH_SUFFIXES include libhdr10plus-rs
+)
+
+find_library(LIBHDR10PLUS_RS_LIBRARY
+    NAMES hdr10plus-rs libhdr10plus-rs hdr10plus_static
+    HINTS ${_HDR10PLUS_PROJECT_HINTS}
+    PATH_SUFFIXES lib lib64
+)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(hdr10plus REQUIRED_VARS LIBHDR10PLUS_RS_LIBRARY LIBHDR10PLUS_RS_INCLUDE_DIR)
+find_package_handle_standard_args(hdr10plus
+    REQUIRED_VARS LIBHDR10PLUS_RS_LIBRARY LIBHDR10PLUS_RS_INCLUDE_DIR
+)
 
-if (hdr10plus_FOUND AND NOT TARGET hdr10plus::hdr10plus)
+if(hdr10plus_FOUND AND NOT TARGET hdr10plus::hdr10plus)
     add_library(hdr10plus::hdr10plus UNKNOWN IMPORTED)
     set_target_properties(hdr10plus::hdr10plus PROPERTIES
         IMPORTED_LOCATION "${LIBHDR10PLUS_RS_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${LIBHDR10PLUS_RS_INCLUDE_DIR}")
+        INTERFACE_INCLUDE_DIRECTORIES "${LIBHDR10PLUS_RS_INCLUDE_DIR}"
+    )
 endif()
 
 set(LIBHDR10PLUS_RS_FOUND ${hdr10plus_FOUND})
 set(LIBHDR10PLUS_RS_LIBRARIES ${LIBHDR10PLUS_RS_LIBRARY})
 mark_as_advanced(LIBHDR10PLUS_RS_INCLUDE_DIR LIBHDR10PLUS_RS_LIBRARY)
+
+if(LIBHDR10PLUS_RS_FOUND)
+    message(STATUS "hdr10plus-rs found: ${LIBHDR10PLUS_RS_LIBRARY}")
+endif()
