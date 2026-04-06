162f1071b7198f5450d319a45525b69f5b0be91b
Patman86 <suckzpsych0@gmail.com>
2026-04-06 14:53:31 +0200

    minor changes

--- Source/App/app_config.c
+++ Source/App/app_config.c
@@ -24,6 +24,7 @@
 #include "app_config.h"
 #include "app_context.h"
 #include "app_input_y4m.h"
+#include "../Lib/Codec/svt_log.h"
 #ifdef _WIN32
 #include <windows.h>
 #include <io.h>
@@ -39,6 +40,10 @@
 
 #if defined(_WIN32)
 #define strcasecmp _stricmp
+#endif
+
+#ifndef _MSC_VER
+#define fscanf_s  fscanf
 #endif
 
 /**********************************
@@ -476,14 +481,14 @@ static EbErrorType set_cfg_fgs_table_path(EbConfig *cfg, const char *token, cons
 #endif
 #ifdef LIBDOVI_FOUND
 static EbErrorType set_cfg_dovi_rpu(EbConfig *cfg, const char *token, const char *value) {
-    printf("Svt[info]: Parsing Dolby Vision RPU file...\n");
+    SVT_INFO("Parsing Dolby Vision RPU file...\n");
     const DoviRpuOpaqueList *rpus = dovi_parse_rpu_bin_file(value);
     if (rpus->error) {
         fprintf(stderr, "%s\n", rpus->error);
         dovi_rpu_list_free(rpus);
         return validate_error(EB_ErrorBadParameter, token, value);
     }
-    printf("Svt[info]: Loaded %zu DoVi RPUs\n", rpus->len);
+    SVT_INFO("Loaded %zu DoVi RPUs\n", rpus->len);
     cfg->dovi_rpus = rpus;
     return EB_ErrorNone;
 }
@@ -491,7 +496,7 @@ static EbErrorType set_cfg_dovi_rpu(EbConfig *cfg, const char *token, const char
 
 #ifdef LIBHDR10PLUS_RS_FOUND
 static EbErrorType set_cfg_hdr10plus_json(EbConfig *cfg, const char *token, const char *value) {
-    printf("Svt[info]: Parsing HDR10+ JSON file...\n");
+    SVT_INFO("Parsing HDR10+ JSON file...\n");
     Hdr10PlusRsJsonOpaque *hdr10plus_json = hdr10plus_rs_parse_json(value);
     const char            *error          = hdr10plus_rs_json_get_error(hdr10plus_json);
     if (error) {
@@ -499,7 +504,7 @@ static EbErrorType set_cfg_hdr10plus_json(EbConfig *cfg, const char *token, cons
         hdr10plus_rs_json_free(hdr10plus_json);
         return validate_error(EB_ErrorBadParameter, token, value);
     }
-    printf("Svt[info]: Loaded HDR10+ JSON file\n");
+    SVT_INFO("Loaded HDR10+ JSON file\n");
     cfg->hdr10plus_json = hdr10plus_json;
     return EB_ErrorNone;
 }
@@ -740,7 +745,7 @@ ConfigDescription config_entry_options[] = {
      "Output compressed (ivf) file path, use `stdout` or `-` to write to pipe"},
 
     {PROGRESS_TOKEN,
-     "Verbosity of the output, default is 2 [0: no progress is printed, 1: basic progress, 2: detailed progress]"},
+     "Verbosity of the output, default is 2 [0: no progress is printed, 1: basic progress, 2: Patman's progress]"},
     {HIDE_BANNER_TOKEN,
      "Do not print out encoder parameters [0: params are printed (Default), 1: no param is printed]"},
 
@@ -1279,11 +1284,12 @@ ConfigDescription fconfig_entry_color_description[] = {
 
     {CONTENT_LIGHT_LEVEL_TOKEN,
      "Set content light level in the format of \"max_cll,max_fall\", refer to the user guide Appendix A.2"},
-// Dolby Vision RPU
 #ifdef LIBDOVI_FOUND
+    // Dolby Vision RPU
     {DOLBY_VISION_RPU_TOKEN, "Set the Dolby Vision RPU path"},
 #endif
 #ifdef LIBHDR10PLUS_RS_FOUND
+    //HDR10+ JSON
     {HDR10PLUS_JSON_TOKEN, "Set the HDR10+ JSON file path"},
 #endif
     // Termination
@@ -2602,7 +2608,7 @@ static EbErrorType read_fgs_table(EbConfig *cfg) {
     film_grain = (AomFilmGrain *)calloc(1, sizeof(AomFilmGrain));
 
     while (!feof(file)) {
-        int num_read = fscanf(file,
+        int num_read = fscanf_s(file,
                               "E %*d %*d %d %hu %d\n",
                               &film_grain->apply_grain,
                               &film_grain->random_seed,
@@ -2618,7 +2624,7 @@ static EbErrorType read_fgs_table(EbConfig *cfg) {
         }
 
         if (film_grain->update_parameters) {
-            num_read = fscanf(file,
+            num_read = fscanf_s(file,
                               "p %d %d %d %d %d %d %d %d %d %d %d %d\n",
                               &film_grain->ar_coeff_lag,
                               &film_grain->ar_coeff_shift,
@@ -2636,72 +2642,72 @@ static EbErrorType read_fgs_table(EbConfig *cfg) {
                 fprintf(stderr, "Unable to read entry header. Read %d != 12\n", num_read);
                 goto fail;
             }
-            if (!fscanf(file, "\tsY %d ", &film_grain->num_y_points)) {
+            if (!fscanf_s(file, "\tsY %d ", &film_grain->num_y_points)) {
                 fprintf(stderr, "Unable to read num y points\n");
                 goto fail;
             }
             for (int i = 0; i < film_grain->num_y_points; ++i) {
                 if (2 !=
-                    fscanf(file, "%d %d", &film_grain->scaling_points_y[i][0], &film_grain->scaling_points_y[i][1])) {
+                    fscanf_s(file, "%d %d", &film_grain->scaling_points_y[i][0], &film_grain->scaling_points_y[i][1])) {
                     fprintf(stderr, "Unable to read y scaling points\n");
                     goto fail;
                 }
             }
-            if (!fscanf(file, "\n\tsCb %d", &film_grain->num_cb_points)) {
+            if (!fscanf_s(file, "\n\tsCb %d", &film_grain->num_cb_points)) {
                 fprintf(stderr, "Unable to read num cb points\n");
                 goto fail;
             }
             for (int i = 0; i < film_grain->num_cb_points; ++i) {
                 if (2 !=
-                    fscanf(file, "%d %d", &film_grain->scaling_points_cb[i][0], &film_grain->scaling_points_cb[i][1])) {
+                    fscanf_s(file, "%d %d", &film_grain->scaling_points_cb[i][0], &film_grain->scaling_points_cb[i][1])) {
                     fprintf(stderr, "Unable to read cb scaling points\n");
                     goto fail;
                 }
             }
-            if (!fscanf(file, "\n\tsCr %d", &film_grain->num_cr_points)) {
+            if (!fscanf_s(file, "\n\tsCr %d", &film_grain->num_cr_points)) {
                 fprintf(stderr, "Unable to read num cr points\n");
                 goto fail;
             }
             for (int i = 0; i < film_grain->num_cr_points; ++i) {
                 if (2 !=
-                    fscanf(file, "%d %d", &film_grain->scaling_points_cr[i][0], &film_grain->scaling_points_cr[i][1])) {
+                    fscanf_s(file, "%d %d", &film_grain->scaling_points_cr[i][0], &film_grain->scaling_points_cr[i][1])) {
                     fprintf(stderr, "Unable to read cr scaling points\n");
                     goto fail;
                 }
             }
 
-            if (fscanf(file, "\n\tcY")) {
+            if (fscanf_s(file, "\n\tcY")) {
                 fprintf(stderr, "Unable to read Y coeffs header (cY)\n");
                 goto fail;
             }
             const int n = 2 * film_grain->ar_coeff_lag * (film_grain->ar_coeff_lag + 1);
             for (int i = 0; i < n; ++i) {
-                if (1 != fscanf(file, "%d", &film_grain->ar_coeffs_y[i])) {
+                if (1 != fscanf_s(file, "%d", &film_grain->ar_coeffs_y[i])) {
                     fprintf(stderr, "Unable to read Y coeffs\n");
                     goto fail;
                 }
             }
-            if (fscanf(file, "\n\tcCb")) {
+            if (fscanf_s(file, "\n\tcCb")) {
                 fprintf(stderr, "Unable to read Cb coeffs header (cCb)\n");
                 goto fail;
             }
             for (int i = 0; i <= n; ++i) {
-                if (1 != fscanf(file, "%d", &film_grain->ar_coeffs_cb[i])) {
+                if (1 != fscanf_s(file, "%d", &film_grain->ar_coeffs_cb[i])) {
                     fprintf(stderr, "Unable to read Cb coeffs\n");
                     goto fail;
                 }
             }
-            if (fscanf(file, "\n\tcCr")) {
+            if (fscanf_s(file, "\n\tcCr")) {
                 fprintf(stderr, "Unable read to Cr coeffs header (cCr)\n");
                 goto fail;
             }
             for (int i = 0; i <= n; ++i) {
-                if (1 != fscanf(file, "%d", &film_grain->ar_coeffs_cr[i])) {
+                if (1 != fscanf_s(file, "%d", &film_grain->ar_coeffs_cr[i])) {
                     fprintf(stderr, "Unable to read Cr coeffs\n");
                     goto fail;
                 }
             }
-            if (fscanf(file, "\n")) {
+            if (fscanf_s(file, "\n")) {
                 // optional newline at end of file,
             }
         }
