5b1d492af363dacf20519dfc44f820f40f0e8c1e
Uranite <62639703+Uranite@users.noreply.github.com>
2026-06-01 17:56:37 +0700

    Add missing tab for alt-cdef terminal output

--- Source/Lib/Globals/enc_settings.c
+++ Source/Lib/Globals/enc_settings.c
@@ -1444,7 +1444,8 @@ void svt_av1_print_lib_params(SequenceControlSet* scs) {
         }
 
         if (config->cdef_level != 0 && config->alt_cdef) {
-            SVT_INFO("SVT [config]: alternative CDEF bias \t\t\t\t\t: %d\n", config->alt_cdef);
+            SVT_INFO("SVT [config]: alternative CDEF bias \t\t\t\t\t\t: %d\n", config->alt_cdef);
+        }
 
         if (config->enable_daala) {
             SVT_INFO("SVT [config]: Daala Dist Level \t\t\t\t\t\t: %d\n", config->enable_daala);
