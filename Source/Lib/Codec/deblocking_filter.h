500e571031f80bae6b1328a95d5b780ff5082536
Uranite <62639703+Uranite@users.noreply.github.com>
2026-06-02 16:12:38 +0700

    Add alt-dlf from Essential

--- Source/Lib/Codec/deblocking_filter.h
+++ Source/Lib/Codec/deblocking_filter.h
@@ -50,7 +50,8 @@ void svt_av1_loop_filter_frame(
         PictureControlSet *pcs,
         /*MacroBlockD *xd,*/ int32_t plane_start, int32_t plane_end/*,
         int32_t partial_frame*/);
-uint64_t picture_sse_calculations(PictureControlSet* pcs, EbPictureBufferDesc* recon_ptr, int32_t plane);
+uint64_t picture_sse_calculations(PictureControlSet* pcs, EbPictureBufferDesc* recon_ptr, int32_t plane,
+                                  double dlf_ac_bias);
 
 EbErrorType svt_av1_pick_filter_level(EbPictureBufferDesc* srcBuffer, // source input
                                       PictureControlSet* pcs, LpfPickMethod method);
